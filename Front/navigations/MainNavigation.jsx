63f97ec12fb94dd510380adc38e70b9d75140e6b
Claudia <marinaclaudia003@gmail.com>
2024-11-16 22:57:00 +0300

    navigate HomeScreen to ListMedication

--- Front/navigations/MainNavigation.jsx
+++ Front/navigations/MainNavigation.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import HomeScreen from '../screens/HomeScreen';
+import React from "react";
+import HomeScreen from "../screens/HomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import MedicationDetailScreen from "../screens/MedicationDetailScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 const MainNavigation = () => {
-    const Stack = createStackNavigator();
+  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-    <Stack.Navigator >
-      <Stack.Screen  options={{ headerShown: false }} name="Login" component={LoginScreen} />
-      <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-      <Stack.Screen options={{ headerShown: false }} name="HomeScreen" component={HomeScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  )
-}
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="MedicationDetailScreen"
+          component={MedicationDetailScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Register"
+          component={RegisterScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="HomeScreen"
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;

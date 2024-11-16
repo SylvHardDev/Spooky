b3f2ea7504995fc3df9485ee135fcd43f9498cab
MiuMiu27 <kiwi.230805@gmail.com>
2024-11-16 21:12:27 +0300

    fix: chatgpt integration

--- Front/navigations/MainNavigation.jsx
+++ Front/navigations/MainNavigation.jsx
@@ -4,12 +4,15 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
+import MedicationDetailScreen  from "../screens/MedicationDetailScreen"
 
 const MainNavigation = () => {
     const Stack = createStackNavigator();
   return (
     <NavigationContainer>
     <Stack.Navigator >
+      
+    <Stack.Screen options={{ headerShown: false }} name="MedicationDetailScreen" component={MedicationDetailScreen} />
       <Stack.Screen  options={{ headerShown: false }} name="Login" component={LoginScreen} />
       <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
       <Stack.Screen options={{ headerShown: false }} name="HomeScreen" component={HomeScreen} />
--- Front/services/chatgptService.js
+++ Front/services/chatgptService.js
@@ -1,22 +1,35 @@
-import { OPENAI_API_KEY} from "@env";
 import axios from "axios";
 
 const openaiEndpoint = "https://api.openai.com/v1/chat/completions";
+
+const apiKey = process.env.EXPO_PUBLIC_OPENAI_API_KEY;
 
 export const sendMessageToChatGPT = async (messages) => {
   try {
     const response = await axios.post(
       openaiEndpoint,
       {
-        model: "gpt-4o",
-        messages: messages, // Un tableau de messages { role: "user/assistant/system", content: "..." }
+        model: "gpt-4o", // ou "gpt-3.5-turbo" selon votre besoin
+        messages: [
+          {
+            role: "system",
+            content:
+              "Vous êtes un assistant spécialisé dans la santé. Vous répondez uniquement aux questions concernant les médicaments (effets secondaires, posologie, interactions, contre-indications) ou les conseils de santé généraux (nutrition, hygiène de vie, exercice physique, sommeil). \
+            Vous ne fournissez jamais de conseils médicaux spécifiques ou de diagnostics. \
+            Répondez toujours en français, quelle que soit la langue de la question. \
+            Ne répondez jamais en utilisant des termes techniques ou médicaux que seuls les professionnels de santé pourraient comprendre. \
+            Ne répondez jamais à des questions qui ne relèvent pas du domaine de la santé, même si elles semblent liées. Si une question est hors du domaine de la santé, indiquez poliment que vous ne pouvez pas répondre car cela ne fait pas partie de vos compétences. \
+            Ne répondez jamais à des questions sensibles ou inappropriées, comme celles relatives au suicide, à l'automutilation ou à tout sujet similaire. Si une telle question est posée, indiquez poliment que vous ne pouvez pas répondre et encouragez la personne à chercher immédiatement de l'aide auprès de professionnels qualifiés ou d'organismes de soutien.",
+          },
+          ...messages,
+        ], // Un tableau de messages { role: "user/assistant/system", content: "..." }
         temperature: 0.7,
       },
       {
         headers: {
-          Authorization: `Bearer ${OPENAI_API_KEY}`,
+          Authorization: `Bearer ${apiKey}`,
           "Content-Type": "application/json",
-        }
+        },
       }
     );
     return response.data.choices[0].message.content;
